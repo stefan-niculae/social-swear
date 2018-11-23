01c7131331f3e8c941656e440efbd9fdf6ea2db0
kendall <kcrouch@usc.edu>
2018-11-22 21:30:45 -0800

    final touchoups on image captioning. nothing special

--- misc/ttools.py
+++ misc/ttools.py
@@ -10,6 +10,17 @@ from dateutil.parser import parse as parse_date
 import requests
 import emoji
 from skimage import io
+import re, string, unicodedata
+
+from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+from stop_words import get_stop_words
+from nltk.corpus import stopwords
+
+import emoji
+from emoji import UNICODE_EMOJI
+
 
 SEARCHCOUNT = 100
 
@@ -669,3 +680,256 @@ def imageColorStats(image_urls):
 	except:
 		return pd.Series([None,None,None])  # some other error
 	return imgAves  # average red pixels in all images, average green, average blue
+
+
+# input str. returns str with links removed
+def strip_links(text):
+    link_regex    = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
+    links         = re.findall(link_regex, text)
+    for link in links:
+        text = text.replace(link[0], ', ')    
+    return text
+
+#global
+cList = {
+  "ain't": "am not",
+  "aren't": "are not",
+  "can't": "cannot",
+  "can't've": "cannot have",
+  "'cause": "because",
+  "could've": "could have",
+  "couldn't": "could not",
+  "couldn't've": "could not have",
+  "didn't": "did not",
+  "doesn't": "does not",
+  "don't": "do not",
+  "hadn't": "had not",
+  "hadn't've": "had not have",
+  "hasn't": "has not",
+  "haven't": "have not",
+  "he'd": "he would",
+  "he'd've": "he would have",
+  "he'll": "he will",
+  "he'll've": "he will have",
+  "he's": "he is",
+  "how'd": "how did",
+  "how'd'y": "how do you",
+  "how'll": "how will",
+  "how's": "how is",
+  "I'd": "I would",
+  "I'd've": "I would have",
+  "I'll": "I will",
+  "I'll've": "I will have",
+  "i'm": "i am",
+  "I've": "I have",
+  "isn't": "is not",
+  "it'd": "it had",
+  "it'd've": "it would have",
+  "it'll": "it will",
+  "it'll've": "it will have",
+  "it's": "it is",
+  "let's": "let us",
+  "ma'am": "madam",
+  "mayn't": "may not",
+  "might've": "might have",
+  "mightn't": "might not",
+  "mightn't've": "might not have",
+  "must've": "must have",
+  "mustn't": "must not",
+  "mustn't've": "must not have",
+  "needn't": "need not",
+  "needn't've": "need not have",
+  "o'clock": "of the clock",
+  "oughtn't": "ought not",
+  "oughtn't've": "ought not have",
+  "shan't": "shall not",
+  "sha'n't": "shall not",
+  "shan't've": "shall not have",
+  "she'd": "she would",
+  "she'd've": "she would have",
+  "she'll": "she will",
+  "she'll've": "she will have",
+  "she's": "she is",
+  "should've": "should have",
+  "shouldn't": "should not",
+  "shouldn't've": "should not have",
+  "so've": "so have",
+  "so's": "so is",
+  "that'd": "that would",
+  "that'd've": "that would have",
+  "that's": "that is",
+  "there'd": "there had",
+  "there'd've": "there would have",
+  "there's": "there is",
+  "they'd": "they would",
+  "they'd've": "they would have",
+  "they'll": "they will",
+  "they'll've": "they will have",
+  "they're": "they are",
+  "they've": "they have",
+  "to've": "to have",
+  "wasn't": "was not",
+  "we'd": "we had",
+  "we'd've": "we would have",
+  "we'll": "we will",
+  "we'll've": "we will have",
+  "we're": "we are",
+  "we've": "we have",
+  "weren't": "were not",
+  "what'll": "what will",
+  "what'll've": "what will have",
+  "what're": "what are",
+  "what's": "what is",
+  "what've": "what have",
+  "when's": "when is",
+  "when've": "when have",
+  "where'd": "where did",
+  "where's": "where is",
+  "where've": "where have",
+  "who'll": "who will",
+  "who'll've": "who will have",
+  "who's": "who is",
+  "who've": "who have",
+  "why's": "why is",
+  "why've": "why have",
+  "will've": "will have",
+  "won't": "will not",
+  "won't've": "will not have",
+  "would've": "would have",
+  "wouldn't": "would not",
+  "wouldn't've": "would not have",
+  "y'all": "you all",
+  "y'alls": "you alls",
+  "y'all'd": "you all would",
+  "y'all'd've": "you all would have",
+  "y'all're": "you all are",
+  "y'all've": "you all have",
+  "you'd": "you had",
+  "you'd've": "you would have",
+  "you'll": "you will",
+  "you'll've": "you will have",
+  "you're": "you are",
+  "you've": "you have"
+}
+
+c_re = re.compile('(%s)' % '|'.join(cList.keys()))
+
+#input str. return str with contractions expanded
+def expandContractions(text, c_re=c_re):
+    def replace(match):
+        return cList[match.group(0)]
+    text = c_re.sub(replace, text)
+    return text
+
+#input str. return str with @mentions removed
+def strip_mentions(text):
+    entity_prefixes = ['@']
+    words = []
+    for word in text.split():
+        word = word.strip()
+        if word:
+            if word[0] not in entity_prefixes:
+                words.append(word)
+    return ' '.join(words)
+
+#input str. return str with hashtags removed
+def strip_hashtags(text):
+    entity_prefixes = ['#']
+    words = []
+    for word in text.split():
+        word = word.strip()
+        if word:
+            if word[0] not in entity_prefixes:
+                words.append(word)
+    return ' '.join(words)
+
+#input str. return str with special chars removed
+def remove_special_characters(text, remove_digits=False):
+    pattern = r'[^a-zA-z0-9\s]' if not remove_digits else r'[^a-zA-z\s]'
+    text = re.sub(pattern, '', text)
+    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    return text
+
+#not sure
+def seperate_emoji(text):
+    entity_prefixes = ['@']
+    for separator in  string.punctuation:
+        if separator not in entity_prefixes :
+            text = text.replace(separator,' ')
+    words = []
+    for word in text.split():
+        word = word.strip()
+        if word:
+            if word[0] not in entity_prefixes:
+                words.append(word)
+    return ' '.join(words)
+
+
+#input str. return pd.Series([polarity,subjectivity])
+def textBlobFeatures(text):
+	snt = TextBlob(text)
+	p = snt.sentiment.polarity
+	s = snt.sentiment.subjectivity
+	return pd.Series([p,s])
+
+#input str. return pd.Series([comp,pos,neu,neg])
+def vaderFeatures(text):
+	analyser = SentimentIntensityAnalyzer()
+	snt = analyser.polarity_scores(text)
+	res = [snt['compound'],snt['pos'],snt['neu'],snt['neg']]
+	return pd.Series(res)
+
+
+#input str and captiopn or tweet type text. return str with all preprocessing done
+def textProcessPipeline(text,textType='caption'):
+	text = text.lower()
+	text = strip_links(text)
+	text = expandContractions(text)
+	if 'tweet' in textType:
+		text = strip_mentions(text)
+		text = strip_hashtags(text)
+	return text
+
+#input str. return tokenized list.
+def tokenizeTextblob(text):
+	return TextBlob(text).words.lemmatize()
+
+#global
+stop_words = list(get_stop_words('en'))         #About 900 stopwords
+nltk_words = list(stopwords.words('english')) #About 150 stopwords
+stop_words.extend(nltk_words)
+
+def remove_stop_words(word_list):
+    output = [w for w in word_list if not w in stop_words]
+    return output
+
+
+#emoji presence for a few emojis
+def eggplant_presence(text):
+	if ':eggplant:' in text:
+		return 1
+	return 0
+
+def mfinger_presence(text):
+	if ':middle_finger:' in text:
+		return 1
+	return 0
+
+def cursing_presence(text):
+	if ':cursing:' in text:
+		return 1
+	return 0
+
+def fuck_presence(text):
+	if ':point_right::ok_hand:' in text:
+		return 1
+	return 0
+
+
+#more text processing
+def textProcessPipelineMore(text,textType='caption'):
+	text = remove_special_characters(text)
+	if 'tweet' in textType:
+		text = seperate_emoji(text)
+	return text
+
